5dc1573cca8386f9418ee2f8d94b97bfe1933803
Stephen <sdarcy29@hotmail.com>
2021-09-08 19:46:39 +0000

    Add aome javascript to generate random numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,29 @@ document.getElementById('how-to').addEventListener("click", function() {
 document.querySelector('.close').addEventListener("click", function() {
     document.querySelector('.bg-modal').style.display = "none";
 });
+
+// Steps
+
+// Generate some random numbers for the computer to receive its pick?
+
+function computerNumber() {
+
+    let pcPick = Math.ceil(Math.random() * 5);
+
+    if (pcPick === 1) {
+        return 'rock';
+    } else if (pcPick === 2) {
+        return 'paper';
+    } else if (pcPick === 3) {
+        return 'scissors';
+    } else if (pcPick === 4) {
+        return 'lizard';
+    } else if (pcPick === 5) {
+        return 'spock'
+    } else {
+        return 'error'
+    }
+
+}
+
+// if statements to decide the winner of each game?
