b131b7dda3b033a77faa09a0e0c8004b3d204ead
Stephen <sdarcy29@hotmail.com>
2021-09-09 19:57:48 +0000

    Add some javascript to project to test functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,42 +23,67 @@ possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click
     getResult()
 }))
 
+// function generateComputerChoice() {
+
+//     let computerChoice = Math.ceil(Math.random() * 5);
+
+//     if (computerChoice === 1) {
+//         return "rock";
+//     } else if (computerChoice === 2) {
+//         return "paper";
+//     } else if (computerChoice === 3) {
+//         return "scissors";
+//     } else if (computerChoice === 4) {
+//         return "lizard";
+//     } else if (computerChoice === 5) {
+//         return "spock";
+//     } else {
+//         return "error";
+//     }
+//     computerChoiceDisplay.innerHTML = computerChoice;
+// }
+
+/**
+ * This function generates a random between 0 and 5 and assigns an icon depending
+ * on the number
+ */
 function generateComputerChoice() {
-    const randomNumber = Math.floor(Math.random() * 5) + 1 // or you can use possibleChoices.length
+    const randomNumber = Math.ceil(Math.random() * 5) // or you can use possibleChoices.length
 
     if (randomNumber === 1) {
         computerChoice = 'rock'
     }
     if (randomNumber === 2) {
+        computerChoice = 'paper'
+    }
+    if (randomNumber === 3) {
         computerChoice = 'scissors'
     }
     if (randomNumber === 3) {
-        computerChoice = 'paper'
+        computerChoice = 'lizard'
+    }
+    if (randomNumber === 3) {
+        computerChoice = 'spock'
     }
     computerChoiceDisplay.innerHTML = computerChoice
 }
 
-function getResult() {
+function getResult(userChoice) {
     if (computerChoice === userChoice) {
-        result = 'its a draw!'
+        result = 'its a draw!';
+    } else {
+        if (userChoice === 'rock' && (computerChoice == 'paper' || computerChoice == 'lizard')) {
+            result = 'you win!'
+        } else if (userChoice === 'paper' && (computerChoice == 'rock' || computerChoice == 'spock')) {
+            result = 'you win!'
+        } else if (userChoice === 'scissors' && (computerChoice == 'paper' || computerChoice == 'lizard')) {
+            result = 'you win!'
+        } else if (userChoice === 'lizard' && (computerChoice == 'spock' || computerChoice == 'paper')) {
+            result = 'you win!'
+        } else if (userChoice === 'spock' && (computerChoice == 'scissors' || computerChoice == 'rock')) {
+            result = 'you win!'
+        }
     }
-    if (computerChoice === 'rock' && userChoice === "paper") {
-        result = 'you win!'
-    }
-    if (computerChoice === 'rock' && userChoice === "scissors") {
-        result = 'you lost!'
-    }
-    if (computerChoice === 'paper' && userChoice === "scissors") {
-        result = 'you win!'
-    }
-    if (computerChoice === 'paper' && userChoice === "rock") {
-        result = 'you lose!'
-    }
-    if (computerChoice === 'scissors' && userChoice === "rock") {
-        result = 'you win!'
-    }
-    if (computerChoice === 'scissors' && userChoice === "paper") {
-        result = 'you lose!'
-    }
+
     resultDisplay.innerHTML = result
 }
